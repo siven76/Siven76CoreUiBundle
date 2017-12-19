e1df1423f6ea34fb42377926b36b2e7c4a08db7a
MageXtensions <support@magextensions.com.au>
2017-12-20 02:09:13 +0400

    Added the bundle class file

---
+++ Siven76CoreUiBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sivensadiyan
+ * Date: 20/12/2017
+ * Time: 02:07
+ */
+
+namespace Siven76\CoreUiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Siven76CoreUiBundle extends Bundle
+{
+
+}
